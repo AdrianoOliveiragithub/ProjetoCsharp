aacf20e8d854f6cdb3ebe25007986862f9cb5552
ADRIANO OLIVEIRA <adrianopxx@gmail.com>
2023-02-11 21:14:22 -0300

    Add files via upload

---
+++ Conexao.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+
+
+namespace SAGI        //baleia
+{
+    class Conexao
+    {
+
+        public int x=10;
+        
+      public MySqlConnection Mcon = new MySqlConnection("Persist Security Info = False; server = localhost;database = teste ;uid = root; pwd= ");
+        public void conectar()
+        {
+            Mcon.Open();
+        }
+
+        public void desconectar()
+        {
+            Mcon.Close();
+        }
+
+        
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,67 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace SAGI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            Conexao cn = new Conexao();
+            try
+            {
+              cn.conectar();
+                string sql = "SELECT * FROM login WHERE login='" + textBox2.Text + "' AND senha ='" + textBox1.Text + "'";
+                MySqlDataAdapter Dados = new MySqlDataAdapter(sql, cn.Mcon);
+                DataTable usuario = new DataTable();  // instanciia a datatable
+                Dados.Fill(usuario); // joga o resultado do data adapter para o datatable;
+                if (usuario.Rows.Count < 1)
+                {
+                    MessageBox.Show("LOGIN OU SENHA INVÁLIDOS!");
+                    textBox1.Clear();
+                    textBox2.Clear();
+
+                } 
+                else
+                {
+                    MessageBox.Show("SEJA BEM VINDO !");
+                    textBox1.Clear();
+                    textBox2.Clear();
+                    Home Tela = new Home();
+                    Tela.Show();
+                }
+
+
+            }
+            catch(Exception E)
+            {
+                MessageBox.Show("erro de conexão!");
+
+            }
+            
+
+        }
+    }
+}
